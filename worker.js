80de4630cee1c0fee57b3d7006ffad8c3622d114
Samuel Lippert <lippertz@gmail.com>
2023-08-16 14:24:05 -0500

    Just merge the files

--- worker.js
+++ worker.js
@@ -1,8 +1,7 @@
 import { Kafka } from '@upstash/kafka'
-import { withDurables } from 'itty-durable'
+import { createDurable, withDurables } from 'itty-durable'
 import { Router, json, error, withParams } from 'itty-router'
 import { SVIX } from 'svix'
-import { TopicManager } from './TopicManager'
 
 let kafkaConfig
 let svixSecret
@@ -95,4 +94,41 @@ export default {
   fetch(request, env) {
     return router.handle(request, env)
   },
+}
+
+export class TopicManager extends createDurable({ autoReturn: true, autoPersist: true }) {
+  constructor(state, env) {
+    super(state, env)
+    this.topics = []
+  }
+
+  listTopics() {
+    return this.topics.map(({ name }) => name)
+  }
+
+  addTopic(name) {
+    this.topics.push({ name })
+  }
+
+  addWebhook(topic, url) {
+    const topicIndex = this.topics.findIndex(({ name }) => name === topic)
+    if (topicIndex === -1) {
+      this.topics.push({ name: topic })
+    }
+    const topicObj = this.topics[topicIndex]
+    if (!topicObj.webhooks) {
+      topicObj.webhooks = []
+    }
+    topicObj.webhooks.push(url)
+  }
+
+  removeWebhook(topic, url) {
+    const topicIndex = this.topics.findIndex(({ name }) => name === topic)
+    if (topicIndex === -1) return
+    const topicObj = this.topics[topicIndex]
+    if (!topicObj.webhooks) return
+    const urlIndex = topicObj.webhooks.findIndex((u) => u === url)
+    if (urlIndex === -1) return
+    topicObj.webhooks.splice(urlIndex, 1)
+  }
 }
