b5a8330ae83463763a0ebed0d3da1bf881b0f286
Samuel Lippert <lippertz@gmail.com>
2023-08-16 12:20:48 -0500

    Add svix boilerplate

--- worker.js
+++ worker.js
@@ -1,7 +1,11 @@
-import { Router, json } from 'itty-router'
 import { Kafka } from '@upstash/kafka'
+import { withDurables } from 'itty-durable'
+import { json, withParams } from 'itty-router'
+import { ThrowableRouter, missing } from 'itty-router-extras'
+import { SVIX } from 'svix'
 
 let kafkaConfig
+let svixSecret
 
 const withCtx = async (request, env) => {
   request.ctx = await env.CTX.fetch(request).then((res) => res.json())
@@ -11,6 +15,7 @@ const withCtx = async (request, env) => {
       username: env.KAFKA_USERNAME,
       password: env.KAFKA_PASSWORD,
     }
+  if (!svixSecret) svixSecret = env.SVIX_SECRET
   console.log(request)
   if (!request.ctx.user) {
     return Response.redirect('/login')
@@ -24,6 +29,7 @@ const withCtx = async (request, env) => {
       listAll: request.ctx.origin + '/',
       consume: request.ctx.origin + '/:queue',
       produce: request.ctx.origin + '/:queue/send/:message',
+      sendBatch: request.ctx.origin + '/:queue/sendBatch',
       acknowledgeAll: request.ctx.origin + '/:queue/ackAll',
       retryAll: request.ctx.origin + '/:queue/retryAll',
       acknowledge: request.ctx.origin + '/:queue/ack/:messageId',
@@ -38,10 +44,42 @@ const withCtx = async (request, env) => {
   }
 }
 
-const router = Router()
+const router = ThrowableRouter()
 router.all('*', withCtx)
+router.all('*', withParams)
+router.all('*', withDurables())
 
-router.get('*', (request) => json({ api: request.api, error: 'Not Found', user: request.ctx.user }, { status: 404 }))
+router.get('/', async (request) => {
+})
+
+router.get('/:queue', async (request) => {
+})
+
+router.get('/:queue/send/:message', async (request) => {
+})
+
+router.post('/:queue/sendBatch', async (request) => {
+})
+
+router.get('/:queue/ackAll', async (request) => {
+})
+
+router.get('/:queue/retryAll', async (request) => {
+})
+
+router.get('/:queue/ack/:messageId', async (request) => {
+})
+
+router.get('/:queue/retry/:messageId', async (request) => {
+})
+
+router.get('/:queue/webhook/:url', async (request) => {
+})
+
+router.get('/:queue/webhook', async (request) => {
+})
+
+router.get('*', (request) => missing({ api: request.api, error: 'Not Found', user: request.ctx.user }))
 
 export default {
   fetch(request, env) {
