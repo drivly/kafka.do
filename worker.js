34d3f3a4bd46a8f25ab91da488a6306796666626
Samuel Lippert <lippertz@gmail.com>
2023-08-22 08:56:28 -0500

    Use REST API

--- worker.js
+++ worker.js
@@ -1,16 +1,8 @@
-import { Kafka } from '@upstash/kafka'
 import { Router, error, json, withParams } from 'itty-router'
-
-let kafka
 
 const withCtx = async (request, env) => {
   request.ctx = await env.CTX.fetch(request).then((res) => res.json())
-  if (!kafka)
-    kafka = new Kafka({
-      url: env.KAFKA_URL,
-      username: env.KAFKA_USERNAME,
-      password: env.KAFKA_PASSWORD,
-    })
+  request.env = env
   if (!request.ctx.user) {
     return Response.redirect('/login')
   }
@@ -30,6 +22,7 @@ const withCtx = async (request, env) => {
     logout: request.ctx.origin + '/logout',
     repo: 'https://github.com/drivly/kafka.do',
   }
+  request.auth = btoa(`${env.UPSTASH_KAFKA_USERNAME}:${env.UPSTASH_KAFKA_PASSWORD}`)
 }
 
 const router = Router()
@@ -37,40 +30,47 @@ router.all('*', withCtx)
 router.all('*', withParams)
 
 router.get('/', async (request) => {
-  return json({ api: request.api, user: request.ctx.user })
+  let data = await fetch(`https://${request.env.UPSTASH_KAFKA_SERVER}/topics`, {
+    headers: {
+      Authorization: 'Basic ' + request.auth,
+    },
+  }).then((response) => response.json())
+  return json({ api: request.api, data, user: request.ctx.user })
 })
 
 router.get('/:queue', async (request) => {
   const { queue } = request.params
-  const c = kafka.consumer()
-  const data = await c.consume({
-    consumerGroupId: 'group_1',
-    instanceId: 'instance_1',
-    topics: [queue],
-    autoOffsetReset: 'earliest',
-  })
-
+  let data = await fetch(`https://${request.env.UPSTASH_KAFKA_SERVER}/consume/GROUP_1/INSTANCE_1/${queue}`, {
+    headers: {
+      Authorization: 'Basic ' + request.auth,
+    },
+  }).then((response) => response.json())
   return json({ api: request.api, data, user: request.ctx.user })
 })
 
 router.get('/:queue/send/:message', async (request) => {
-  const p = kafka.producer()
   const { queue, message } = request.params
-  const data = await p.produce(queue, message)
+  let data = await fetch(`https://${request.env.UPSTASH_KAFKA_SERVER}/produce/${queue}/${message}`, {
+    headers: {
+      Authorization: 'Basic ' + request.auth,
+    },
+  }).then((response) => response.json())
+
   return json({ api: request.api, data, user: request.ctx.user })
 })
 
 router.post('/:queue/sendBatch', async (request) => {
   const { queue } = request.params
   const messages = await request.json()
-  const p = kafka.producer()
-  const options = undefined
-  const data = await p.produceMany(messages.map((value) => ({ topic: queue, value, options })))
-  return json({ api: request.api, data, user: request.ctx.user })
-})
+  let data = await fetch(`https://${request.env.UPSTASH_KAFKA_SERVER}/produce/${queue}`, {
+    headers: {
+      Authorization: 'Basic ' + request.auth,
+    },
+    method: 'POST',
+    body: JSON.stringify(messages.map((value) => ({ value }))),
+  }).then((response) => response.json())
 
-router.get('/:queue/retry/:messageId', async (request) => {
-  return json({ api: request.api, user: request.ctx.user })
+  return json({ api: request.api, data, user: request.ctx.user })
 })
 
 router.get('*', (request) => error(404, { api: request.api, error: 'Not Found', user: request.ctx.user }))
