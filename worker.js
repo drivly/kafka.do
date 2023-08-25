b6d800210c35278d385da39e081cb6a866c0c7e7
sam-lippert <samuel@driv.ly>
2023-08-25 02:40:30 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -78,4 +78,3 @@ export default {
 export { UpstashKafka } from './UpstashKafka'
 export { QueueConsumer, default as Consumer } from './consumer'
 export { QueueProducer } from './producer'
-export { Message, MessageBatch, MessageSendRequest, QueuesContentType } from './message'
