95816aea8984a330a51fa8bd16e71ebb66e424f4
Samuel Lippert <lippertz@gmail.com>
2023-09-19 20:59:42 -0500

    Check array

--- producer.js
+++ producer.js
@@ -9,7 +9,7 @@ export const QueueProducer = (queue, env) => {
         return await env[queue].kafka.send(message.body || message)
       },
       sendBatch: async (messages) => {
-        return await env[queue].kafka.sendBatch(messages.map((m) => m.body || m))
+        return await env[queue].kafka.sendBatch(Array.isArray(messages) ? messages.map((m) => m.body || m) : messages.body || messages)
       },
     }
   }
